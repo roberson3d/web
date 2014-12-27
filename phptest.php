668fd730bc896148685ccca8b94f8ac996ccae4c
Ricky Roberson <Franklin@Rickys-MacBook-Pro.local>
2014-12-27 16:03:47 -0600

    php test

---
+++ phptest.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<!-- BC_OBNW -->
+<head>
+<title>Ricky Roberson | Portfolio</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<link href="/css/ModuleStyleSheets.css" type="text/css" rel="StyleSheet" />
+<script type="text/javascript">var jslang='EN';</script>
+<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400,300,700" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="css/base.css">
+<link rel="shortcut icon" href="favicon.ico?v=2" />
+<meta charset="utf-8" />
+<meta name="description" content="">
+<meta name="author" content="">
+<meta name="HandheldFriendly" content="true">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<!-- Import CSS -->
+<!--[if lt IE 10]>
+		<link href= "css/ie.css" rel= "stylesheet" media= "all" />
+	<![endif]-->
+<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+<p>Hello World</p>
+<?php echo '<p>Hello World</p>'; ?>
+
+</body>
+</html>
